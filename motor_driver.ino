20996b199b01706b264d1bbb6219be836880ae16
plasmagen <41835786+plasmagen@users.noreply.github.com>
2018-09-06 17:14:21 +0530

    Add files via upload

---
+++ motor_driver.ino
@@ -0,0 +1,48 @@
+
+const int leftForward = 5;
+const int leftBackward = 4;
+const int rightForward = 0;
+const int rightBackward = 2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(leftForward,OUTPUT);
+  pinMode(leftBackward,OUTPUT);
+  pinMode(rightForward,OUTPUT);
+  pinMode(rightBackward,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    //  forward
+  digitalWrite(leftForward,HIGH);
+  digitalWrite(leftBackward,LOW);
+  digitalWrite(rightForward,HIGH);
+  digitalWrite(rightBackward,LOW);
+  delay(1000);
+  //  left
+  digitalWrite(leftForward,LOW);
+  digitalWrite(leftBackward,LOW);
+  digitalWrite(rightForward,HIGH);
+  digitalWrite(rightBackward,LOW);
+  delay(1000);
+  
+  //  forward
+  digitalWrite(leftForward,HIGH);
+  digitalWrite(leftBackward,LOW);
+  digitalWrite(rightForward,LOW);
+  digitalWrite(rightBackward,LOW);
+  delay(1000);
+  
+  //  backward
+  digitalWrite(leftForward,LOW);
+  digitalWrite(leftBackward,HIGH);
+  digitalWrite(rightForward,LOW);
+  digitalWrite(rightBackward,HIGH);
+  delay(1000);
+
+
+}
